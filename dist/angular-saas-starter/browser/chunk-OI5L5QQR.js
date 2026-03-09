806d7e269fb4f31a5a6d363c1299cddede1f3cac
Mahmoud <m.samir@fuinco.com>
2026-03-09 23:10:50 +0200

    fix dependency versions for vercel

---
+++ dist/angular-saas-starter/browser/chunk-OI5L5QQR.js
@@ -0,0 +1,1 @@
+import{K as r,U as o,Z as s,e as n,ea as u}from"./chunk-NGDIBKQX.js";function c(t){t||(s(c),t=o(u));let i=new n(e=>t.onDestroy(e.next.bind(e)));return e=>e.pipe(r(i))}export{c as a};
